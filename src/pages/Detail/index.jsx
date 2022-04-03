f173549dcfd95a3305037e5c0ef9b82f79e35ce1
jyun <you@example.com>
2022-04-02 23:43:24 -0400

    feat: detail component styles

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -12,13 +12,13 @@ export const Detail = ({ setActive }) => {
   return (
     <section className={'Detail-container'}>
       <header className={'Detail-header'}><h2>Detail Year</h2></header>
-      <nav className='Detail-nav'  >
-        <div>Date</div>
-        <div>Description</div>
-        <div>Incomes</div>
-        <div>Expenses</div>
-        <div>Saves</div>
-        <div>Balance</div>
+      <nav className='Detail-nav'>
+        <div> <h3>Date</h3></div>
+        <div> <h3>Description</h3></div>
+        <div> <h3>Debit</h3> </div>
+        <div> <h3>Credit</h3></div>
+        <div> <h3>Saves</h3> </div>
+        <div> <h3>Total</h3></div>
       </nav>
 
       <main className='Detail-table'>
@@ -27,41 +27,41 @@ export const Detail = ({ setActive }) => {
           <div className='Detail-table-title'>
             <h3>Enero</h3>
           </div>
-
           <div className='Detail-table-detail'>
-            <div>02/04/2022(fecha)</div>
-            <div>Entrada por pago de salario(descripcion)</div>
-            <div>1000 $ bs (Incomes)</div>
+            <div>02/04/2022 {/*fecha*/}</div>
+            <div>Entrada por pago de salario{/* decripcion  */}</div>
+
+            <div>1000 $ {/* Incomes */}</div>
             <div></div>
             <div></div>
             <div></div>
           </div>
 
           <div className='Detail-table-detail' >
-            <div>02/04/2022(fecha)</div>
-            <div>pago servicios(descripcion)</div>
+            <div>02/04/2022{/* fecha */}</div>
+            <div>pago servicios{/* decripcion  */}</div>
             <div></div>
-            <div>200 $ bs (Expenses)</div>
+            <div>200 $  {/* Expenses */}</div>
             <div></div>
             <div></div>
           </div>
 
           <div className='Detail-table-detail'>
-            <div>02/04/2022(fecha)</div>
-            <div>Ahorro(descripcion)</div>
+            <div>02/04/2022{/* fecha */}</div>
+            <div>Ahorro{/* descripcion  */}</div>
             <div></div>
             <div></div>
-            <div>300 $ bs (Incomes)</div>
+            <div>300 $  {/* saves  */}</div>
             <div></div>
           </div>
 
-          <div className='Detail-table-detail'>
+          <div className='Detail-table-total'>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
             <div>Ahorros</div>
-            <div>Balance diferencia (I - (Ex + Ahorro))</div>
+            <div>Total{/*  Balance diferencia (I - (Ex + Ahorro))*/}</div>
           </div>
         </div>
 
@@ -73,49 +73,263 @@ export const Detail = ({ setActive }) => {
           </div>
 
           <div className='Detail-table-detail'>
-            <div>02/04/2022(fecha)</div>
-            <div>Entrada por pago de salario(descripcion)</div>
-            <div>1000 $ bs (Incomes)</div>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
             <div></div>
             <div></div>
             <div></div>
           </div>
 
           <div className='Detail-table-detail' >
-            <div>02/04/2022(fecha)</div>
-            <div>pago servicios(descripcion)</div>
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
             <div></div>
-            <div>200 $ bs (Expenses)</div>
+            <div>200 $</div>
             <div></div>
             <div></div>
           </div>
 
           <div className='Detail-table-detail'>
-            <div>02/04/2022(fecha)</div>
-            <div>Ahorro(descripcion)</div>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
             <div></div>
             <div></div>
-            <div>300 $ bs (Incomes)</div>
+            <div>300 $ </div>
             <div></div>
           </div>
 
-          <div className='Detail-table-detail'>
+          <div className='Detail-table-total'>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
             <div>Ahorros</div>
-            <div>Balance diferencia (I - (Ex + Ahorro))</div>
+            <div>Total</div>
           </div>
         </div>
 
-        {/* Tabla3  */}
+        {/* tabla2  */}
+        <div className='Detail-table-container'>
+          <div className='Detail-table-title'>
+            <h3>Febrero</h3>
+          </div>
 
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail' >
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
+            <div></div>
+            <div>200 $</div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
+            <div></div>
+            <div></div>
+            <div>300 $ </div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-total'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Ahorros</div>
+            <div>Total</div>
+          </div>
+        </div>
+
+        {/* tabla2  */}
+        <div className='Detail-table-container'>
+          <div className='Detail-table-title'>
+            <h3>Febrero</h3>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail' >
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
+            <div></div>
+            <div>200 $</div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
+            <div></div>
+            <div></div>
+            <div>300 $ </div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-total'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Ahorros</div>
+            <div>Total</div>
+          </div>
+        </div>
+
+        {/* tabla2  */}
+        <div className='Detail-table-container'>
+          <div className='Detail-table-title'>
+            <h3>Febrero</h3>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail' >
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
+            <div></div>
+            <div>200 $</div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
+            <div></div>
+            <div></div>
+            <div>300 $ </div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-total'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Ahorros</div>
+            <div>Total</div>
+          </div>
+        </div>
+
+        {/* tabla2  */}
+        <div className='Detail-table-container'>
+          <div className='Detail-table-title'>
+            <h3>Febrero</h3>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail' >
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
+            <div></div>
+            <div>200 $</div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
+            <div></div>
+            <div></div>
+            <div>300 $ </div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-total'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Ahorros</div>
+            <div>Total</div>
+          </div>
+        </div>
+
+        {/* tabla2  */}
+        <div className='Detail-table-container'>
+          <div className='Detail-table-title'>
+            <h3>Febrero</h3>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Entrada por pago de salario</div>
+            <div>1000 $ </div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail' >
+            <div>02/04/2022</div>
+            <div>pago servicios</div>
+            <div></div>
+            <div>200 $</div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-detail'>
+            <div>02/04/2022</div>
+            <div>Ahorro</div>
+            <div></div>
+            <div></div>
+            <div>300 $ </div>
+            <div></div>
+          </div>
+
+          <div className='Detail-table-total'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Ahorros</div>
+            <div>Total</div>
+          </div>
+        </div>
       </main>
-
-      <div>
-        <div>(date)</div>
-        <div>(Description)</div>
+      <div className='Detail-total-title'>
+        <h3>Year total</h3>
+      </div>
+      <div className='Detail-total'>
+        <div></div>
+        <div></div>
         <div>Total Icomes</div>
         <div>Total Expenses</div>
         <div>Total Ahorros</div>
