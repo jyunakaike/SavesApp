8b06ac460ecc9984a7de031dae27eda68cbf3356
jyun <you@example.com>
2022-04-02 20:19:10 -0400

    feat: navbar layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header } from '../Header';
+import { Footer } from '../Footer';
+import { Navbar } from '../Navbar';
 
 const Layout = ({ children }) => {
     return (
         <header>
             <Header />
             {children}
+            <Navbar />
+
         </header>
     )
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+// React Icons
+import { RiMoneyDollarCircleFill } from 'react-icons/ri';
+import { IoAnalyticsOutline } from 'react-icons/io5';
+import { FaMoneyCheckAlt } from 'react-icons/fa';
+
+import "./style.css";
+
+export const Navbar = () => {
+    const [active, setActive] = useState("2");
+
+    const clickNav = (item) => {
+        if(item !== active){
+            setActive(item);
+        }
+    }
+
+    return (
+        <nav className='Navbar-container ' >
+            <ul>
+                <li className={`List ${(active === "1")?"active":""}`} onClick={()=> { clickNav("1")}}> <Link to="/"> <RiMoneyDollarCircleFill className='Navbar-icon' /> <p>Incomes</p></Link>  </li>
+                <li className={`List ${(active === "2")?"active":""}`} onClick={()=> { clickNav("2")}}> <Link to="/"> <IoAnalyticsOutline className='Navbar-icon' /> <p> Detail</p> </Link> </li>
+                <li className={`List ${(active === "3")?"active":""}`} onClick={()=> { clickNav("3")}}> <Link to="/"> <FaMoneyCheckAlt className='Navbar-icon' /> <p>Expenses</p> </Link>  </li>
+
+                <div className='Indicator'></div>
+            </ul>
+        </nav>
+    )
+}
