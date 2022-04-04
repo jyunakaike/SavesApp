1d90943ea8eb9419d4a461cabf945b6df759f790
jyun <you@example.com>
2022-04-03 21:16:44 -0400

    feat: expense components

--- src/pages/Expense/index.jsx
+++ src/pages/Expense/index.jsx
@@ -1,13 +1,31 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
+import "./style.css";
 
 export const Expense = ({ setActive }) => {
-  
+
   useEffect(() => {
     setActive("3")
   }, []);
 
 
   return (
-    <div>Expense</div>
+    <section className={'Expense-container-section'}>
+      <main>
+        <header className={'Expense-header'}>
+          <h2>Add Expense</h2> 
+        </header>
+        <div className={'Expense-container'}>
+          <div className={'Expense-container-detail'}>
+            <div><h3>Funds (Expense)</h3></div>
+            <div><input type="text" /></div>
+            <div><h3>Description</h3></div>
+            <div><input type="text" /></div>
+          </div>
+          <div className={'Expense-container-save'}>
+            <button>Save</button>
+          </div>
+        </div>
+      </main>
+    </section>
   )
 }
